69716ec9194a1680b197ebe50e57cd22dc3a6d29
ignacioLopezDev <ignaciolopez@outlook.com>
2022-09-23 09:04:49 -0300

    new commit

--- client/src/components/TasksList.js
+++ client/src/components/TasksList.js
@@ -1,24 +1,76 @@
-import { useEffect } from "react"
-
+import { useEffect, useState } from "react";
+import { Button, Card, CardContent, Typography } from "@mui/material";
 
 export default function TaskList() {
+  const [tasks, setTasks] = useState([]);
 
-const loadTask = async () => {
-  const res = await fetch("http://localhost:3000/routes/tasks")
-  const data = await res.json()
-  
+  // cargar tareas, para utilizarlo en el useEffect
+  const loadTask = async () => {
+    const res = await fetch("http://localhost:3000/routes/tasks");
+    const data = await res.json();
+    setTasks(data);
+  };
+
+  // useEffect
+  useEffect(() => {
+    loadTask();
+  }, []);
+
+// HandleDelete para eliminar tarea
+const delete = async () => {
+  const res = await fetch("http://localhost:3000/routes/tasks/"),{
+    method: "DELETE"
+  }
 }
 
-useEffect(() => {
-
-}, [])
-
-
+const handleDelete = (e => {})
 
   return (
     <>
-    <h1>Task List</h1>
+      <h1>Task List</h1>
+      {tasks.map((task) => (
+        <Card
+          style={{
+            color: "white",
+            marginBottom: ".7rem",
+            backgroundColor: "#1e272e",
+          }}
+        >
+          <CardContent
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <div>
+              <Typography style={{ color: "whitesmoke" }}>
+                {task.Title}
+              </Typography>
+              <Typography style={{ color: "grey" }}>
+                {task.Description}
+              </Typography>
+            </div>
+            <div>
+              <Button
+                style={{color:"black"}}
+                variant="contained"
+                color="inherit"
+                onClick={() => console.log("eliminando")}
+              >
+                Edit
+              </Button>
+              <Button
+                variant="contained"
+                color="warning"
+                style={{marginLeft:".5rem"}}
+                onClick={() => console.log("delete")}
+              >
+                Delete
+              </Button>
+            </div>
+          </CardContent>
+        </Card>
+      ))}
     </>
- 
-  )
+  );
 }
